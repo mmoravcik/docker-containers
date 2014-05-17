d2e5fe828e10d3ad2534e7f5cdf4658666298d03
Matus Moravcik <matus.moravcik@gmail.com>
2014-05-17 16:08:30 +0100

    Add supervisor and ability to override creds

--- postgres/scripts/start.sh
+++ postgres/scripts/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Start postgres
-/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
+mkdir -p /etc/postgresql /var/lib/postgresql /var/log/postgresql /var/log/supervisor
+chown -R postgres:postgres /etc/postgresql /var/lib/postgresql /var/log/postgresql
+
+function db_command {
+    echo $@ | su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
+}
+
+db_command "CREATE USER $USERNAME WITH SUPERUSER PASSWORD '$PASSWORD';"
+db_command "CREATE DATABASE $DB OWNER $USERNAME;"
+
+supervisord -n
+
